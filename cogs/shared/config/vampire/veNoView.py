aeb9557fd83b252cf1e34b3c23d5612a998e9c11
AshenEntropy <ashen_entropy@proton.me>
2023-11-07 15:31:49 -0500

    removed excess stuff left over from me changing the way the vampireRoller works

--- cogs/shared/config/vampire/veNoView.py
+++
@@ -1,6 +0,0 @@
-import discord
-from misc import ashen_utils as au
-
-
-# Embeds
-no_view_embed = discord.Embed(title='Invalid', description='Try Again, invalid `View`', color=au.embed_colors["red"])
