24a875b5618650959af2f593d28ef8cad956f977
AshenEntropy <ashen_entropy@proton.me>
2023-11-12 21:48:52 -0500

    minor fixes/changes

--- client_config.py
+++ client_config.py
@@ -2,4 +2,4 @@
 import os
 TOKEN: str = f'{os.environ["TOKEN"]}'  # ? Find this on your discord developer portal
 PREFIX: str = f'ex.'  # ? Prefix for all Non-Slash Commands.
-SLASH_MODE: bool = False  # ? I set this to false when doing lots of testing
+SLASH_MODE: bool = True  # ? I set this to False when doing lots of testing
