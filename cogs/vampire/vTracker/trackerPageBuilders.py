a31db5291d68353c0d43accd29a1034ade6006d2
AshenEntropy <ashen_entropy@proton.me>
2023-12-21 05:18:05 -0500

    tevNav v0.2

--- cogs/vampire/vTracker/trackerPageBuilders.py
+++ cogs/vampire/vTracker/trackerPageBuilders.py
@@ -1,5 +1,5 @@
-import cogs.vampire.vTracker.trackerResources as tR
 import cogs.vampire.vTracker.trackerViews as tV
+import misc.config.mainConfig as mC
 
 
 async def homePageBuilder(return_embed):
@@ -11,12 +11,12 @@ async def homePageBuilder(return_embed):
 async def hpwpPageBuilder(return_embed, cursor):
     # ! Needs a hp/wp add/remove
     # ? hc = health_count | wpc = willpower_count
-    hc_full: str = str(tR.health_full_emoji * int(cursor.execute('SELECT healthBase from health').fetchone()[0]))
-    hc_sup: str = str(tR.health_sup_emoji * int(cursor.execute('SELECT healthSUP from health').fetchone()[0]))
-    hc_agg: str = str(tR.health_agg_emoji * int(cursor.execute('SELECT healthAGG from health').fetchone()[0]))
-    wpc_full: str = str(tR.willpower_full_emoji * int(cursor.execute('SELECT willpowerBase from willpower').fetchone()[0]))
-    wpc_sup: str = str(tR.willpower_sup_emoji * int(cursor.execute('SELECT willpowerSUP from willpower').fetchone()[0]))
-    wpc_agg: str = str(tR.willpower_agg_emoji * int(cursor.execute('SELECT willpowerAGG from willpower').fetchone()[0]))
+    hc_full: str = str(mC.health_full_emoji * int(cursor.execute('SELECT healthBase from health').fetchone()[0]))
+    hc_sup: str = str(mC.health_sup_emoji * int(cursor.execute('SELECT healthSUP from health').fetchone()[0]))
+    hc_agg: str = str(mC.health_agg_emoji * int(cursor.execute('SELECT healthAGG from health').fetchone()[0]))
+    wpc_full: str = str(mC.willpower_full_emoji * int(cursor.execute('SELECT willpowerBase from willpower').fetchone()[0]))
+    wpc_sup: str = str(mC.willpower_sup_emoji * int(cursor.execute('SELECT willpowerSUP from willpower').fetchone()[0]))
+    wpc_agg: str = str(mC.willpower_agg_emoji * int(cursor.execute('SELECT willpowerAGG from willpower').fetchone()[0]))
 
     return_embed.add_field(name='Health', value=f'{hc_full}{hc_sup}{hc_agg}', inline=False)
     return_embed.add_field(name='Willpower', value=f'{wpc_full}{wpc_sup}{wpc_agg}', inline=False)
@@ -28,7 +28,7 @@ async def hungerPageBuilder(return_embed, cursor):
     # ! Needs a Hunt Button (Lets use Selected Pool or Pred Pool)
     hunger: int = int(cursor.execute('SELECT hunger from charInfo').fetchone()[0])
     pred_type: str = str(cursor.execute('SELECT predator_type from charInfo').fetchone()[0])
-    return_embed.add_field(name='Hunger', value=f'{hunger * tR.dot_full_emoji} {abs(hunger - 5) * tR.dot_empty_emoji}', inline=False)
+    return_embed.add_field(name='Hunger', value=f'{hunger * mC.dot_full_emoji} {abs(hunger - 5) * mC.dot_empty_emoji}', inline=False)
     return_embed.add_field(name='Predator Type', value=f'{pred_type}', inline=False)
     return_view = tV.KTV_HUNGER
     return return_embed, return_view
@@ -45,7 +45,7 @@ async def attributePageBuilder(return_embed, cursor):
         if for_var / 3 in (1, 2):
             return_embed.add_field(name='', value='', inline=False)
         count = int(cursor.execute(f'SELECT {attributes[for_var].lower()} from charAttributes').fetchone()[0])
-        emojis = f'{count * tR.dot_full_emoji} {abs(count - 5) * tR.dot_empty_emoji}'
+        emojis = f'{count * mC.dot_full_emoji} {abs(count - 5) * mC.dot_empty_emoji}'
 
         return_embed.add_field(name=f'{attributes[for_var]}', value=f'{emojis}', inline=True)
         for_var += 1
@@ -68,7 +68,7 @@ async def physicalSkillsPageBuilder(return_embed, cursor):
             return_embed.add_field(name='', value='', inline=False)
         count = int(
             cursor.execute(f'SELECT {physical_skills[for_var].lower()} from physicalSkills').fetchone()[0])
-        emojis = f'{count * tR.dot_full_emoji} {abs(count - 5) * tR.dot_empty_emoji}'
+        emojis = f'{count * mC.dot_full_emoji} {abs(count - 5) * mC.dot_empty_emoji}'
 
         return_embed.add_field(name=f'{physical_skills[for_var]}', value=f'{emojis}', inline=True)
         for_var += 1
@@ -84,7 +84,7 @@ async def socialSkillsPageBuilder(return_embed, cursor):
             return_embed.add_field(name='', value='', inline=False)
         count = int(
             cursor.execute(f'SELECT {social_skills[for_var].lower()} from socialSkills').fetchone()[0])
-        emojis = f'{count * tR.dot_full_emoji} {abs(count - 5) * tR.dot_empty_emoji}'
+        emojis = f'{count * mC.dot_full_emoji} {abs(count - 5) * mC.dot_empty_emoji}'
 
         return_embed.add_field(name=f'{social_skills[for_var]}', value=f'{emojis}', inline=True)
         for_var += 1
@@ -100,7 +100,7 @@ async def mentalSkillsPageBuilder(return_embed, cursor):
             return_embed.add_field(name='', value='', inline=False)
         count = int(
             cursor.execute(f'SELECT {mental_skills[for_var].lower()} from mentalSkills').fetchone()[0])
-        emojis = f'{count * tR.dot_full_emoji} {abs(count - 5) * tR.dot_empty_emoji}'
+        emojis = f'{count * mC.dot_full_emoji} {abs(count - 5) * mC.dot_empty_emoji}'
 
         return_embed.add_field(name=f'{mental_skills[for_var]}', value=f'{emojis}', inline=True)
         for_var += 1
@@ -116,7 +116,7 @@ async def disciplinePageBuilder(return_embed, cursor):
         if for_var / 3 in (1, 2):
             return_embed.add_field(name='', value='', inline=False)
         count = int(cursor.execute(f'SELECT {disciplines[for_var].lower()} from disciplines').fetchone()[0])
-        emojis = f'{count * tR.dot_full_emoji} {abs(count - 5) * tR.dot_empty_emoji}'
+        emojis = f'{count * mC.dot_full_emoji} {abs(count - 5) * mC.dot_empty_emoji}'
 
         if count <= 0:
             pass
@@ -142,12 +142,12 @@ async def extrasPageBuilder(return_embed, cursor):
 
     return_embed.add_field(name='', value=f'', inline=False)
 
-    return_embed.add_field(name='Generation', value=f'{gen * tR.dot_full_emoji}', inline=True)
-    return_embed.add_field(name='Blood Potency', value=f'{bp * tR.dot_full_emoji}', inline=True)
+    return_embed.add_field(name='Generation', value=f'{gen * mC.dot_full_emoji}', inline=True)
+    return_embed.add_field(name='Blood Potency', value=f'{bp * mC.dot_full_emoji}', inline=True)
 
     return_embed.add_field(name='', value=f'', inline=False)
 
-    return_embed.add_field(name='Humanity', value=f'{humanity * tR.dot_full_emoji} {stains * tR.dot_empty_emoji}', inline=True)
+    return_embed.add_field(name='Humanity', value=f'{humanity * mC.dot_full_emoji} {stains * mC.dot_empty_emoji}', inline=True)
     return_embed.add_field(name='Path of Enlightenment', value=f'{path_of_enlightenment}', inline=True)
     return_view = tV.KTV_EXTRA
     return return_embed, return_view
