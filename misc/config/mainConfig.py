eab4b636d117f248c9827b536d225d0138759e49
AshenEntropy <ashen_entropy@proton.me>
2023-12-21 05:16:52 -0500

    moved emojis to mainConfig

--- misc/config/mainConfig.py
+++ misc/config/mainConfig.py
@@ -12,6 +12,19 @@ embed_colors = {
 }
 placeholder_img = 'https://cdn.discordapp.com/attachments/1112082402077052939/1186553925978042418/xBmTDu3.png?ex=6593ab75&is=65813675&hm=57765ab8a90864f4f85807fd5812d95ec4962ab644c6ea9ba05ed4b2efa145e9&'
 
+# ? Bot Specific Emojis
+willpower_full_emoji = ' <:full_wp:1186586608712028160> '
+willpower_sup_emoji = ' <:sup_wp:1186586616798650470> '
+willpower_agg_emoji = ' <:agg_wp:1186586594568851506> '
+
+health_full_emoji = ' <:full_hp:1186583370382188574> '
+health_sup_emoji = ' <:sup_hp:1186583396068102154> '
+health_agg_emoji = ' <:agg_hp:1186583405631123538> '
+
+dot_full_emoji = ' <:dot_full:1187292963164926013>  '
+dot_empty_emoji = ' <:dot_empty:1187292937650970665> '
+
+
 # ? Should be changed
 RUNNER: str = f'.ashywinter'  # ? YOUR DISCORD NAME
 RUNNER_ID: int = 567819777209532418  # ? YOUR DISCORD ID as an INTEGER
