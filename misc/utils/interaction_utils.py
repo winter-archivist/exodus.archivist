bb1227f466d943ff07f61704ff907fc59b65f0c1
AshenEntropy <ashen_entropy@proton.me>
2023-12-23 00:14:19 -0500

    removed unused file

--- misc/utils/interaction_utils.py
+++
@@ -1,9 +0,0 @@
-import discord
-
-
-async def interactionExpirationChecker(interaction: discord.Interaction):
-    if interaction.is_expired() is True:
-        await interaction.response.send_message(content='> This Command has Expired', ephemeral=True)
-        return
-    elif interaction.is_expired() is False:
-        pass
