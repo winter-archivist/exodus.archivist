b8bbc2ee3c5e489f410871e8aea3222e35b5eb8d
AshenEntropy <ashen_entropy@proton.me>
2024-02-16 19:18:51 -0500

    Character Files Fragmented; Getting/Updating Info works.

--- cogs/vtm_toolbox/vampireToolboxCog.py
+++ cogs/vtm_toolbox/vampireToolboxCog.py
@@ -1,12 +1,14 @@
-import discord
-from discord import Embed, app_commands
-from discord.ext import commands
-
+import os
+import json
 import sqlite3
-import os as os
+import discord
 from zenlog import log
+from discord.ext import commands
+from discord import Embed, app_commands
 
 from misc.config import mainConfig as mC
+
+import cogs.vtm_toolbox.vtb_characters.vtb_character_manager as vtb_cm
 
 import cogs.vtm_toolbox.vtb_misc.vtbUtils as vU
 import cogs.vtm_toolbox.vtb_misc.vtbPageSystem as vPS
@@ -16,32 +18,26 @@ class VampireRoll(commands.Cog):
     def __init__(self, CLIENT):
         self.CLIENT = CLIENT
 
-    @app_commands.command(name='vampire-roll', description='VTM v5 Dice Roller!')
-    @app_commands.describe(charactername='Character Name')
-    async def VampireRoll(self, interaction: discord.Interaction, charactername: str):
-        await vU.rollPrep(interaction, charactername)
-        await vPS.vampirePageCommand(self, interaction, charactername, 'roller.difficulty', False)
-
-    @app_commands.command(name='vampire-tracker', description='VTM v5 Character Tracker!')
-    @app_commands.describe(character_name='Character Name')
-    async def VampireTracker(self, interaction: discord.Interaction, character_name: str):
-        await vPS.vampirePageCommand(self, interaction, character_name, 'tracker.home', True)
-
     @app_commands.command(name='dev-test', description='---DO NOT TOUCH---')
     @app_commands.describe(character_name='Character Name')
-    async def DevTest(self, interaction: discord.Interaction, character_name: str):
-        import cogs.vtm_toolbox.vtb_characters.vtb_character_manager as vtb_cm
-        import json
+    @app_commands.describe(make='Make New Files')
+    async def DevTestNEWTRACKER(self, interaction: discord.Interaction, character_name: str, make: bool = False):
+
+        if interaction.user.id != mC.RUNNER_ID:
+            log.crit(f'{interaction.user.name} used the no touch.')
+            dev_test_embed = discord.Embed(title='NO TOUCH!', description='NO TOUCH!', color=mC.EMBED_COLORS['red'])
+            await interaction.response.send_message(embed=dev_test_embed)
+            return
+
+        if make:
+            await vtb_cm.make_character_files(interaction, character_name)
 
         CHARACTER_NAME_FILE: str = f'cogs/vtm_toolbox/vtb_characters/{interaction.user.id}/target_character.json'
         CHARACTER_NAME_DICT: dict = {'character_name': character_name}
-
         with open(CHARACTER_NAME_FILE, "w") as operate_file:
             json.dump(CHARACTER_NAME_DICT, operate_file)
 
-        await vtb_cm.make_character_file(interaction, character_name)
-
-        character_class = vtb_cm.vtb_CharacterManager(interaction)
+        character_class = vtb_cm.vtb_Character(interaction)
 
         dev_test_embed = discord.Embed(title='`!__DEV__DEBUG__TESTS__!`', description='`!__ONLY__PRESS__THINGS__IF__INSTRUCTED__!`', color=mC.EMBED_COLORS['red'])
         dev_test_embed.add_field(name='Char Name', value=f'{character_class.CHARACTER_NAME}', inline=True)
@@ -49,112 +45,6 @@ class VampireRoll(commands.Cog):
         dev_test_embed.add_field(name='Interactor ID', value=f'{interaction.user.id}', inline=True)
 
         await interaction.response.send_message(embed=dev_test_embed, view=vtb_cm.vtb_DEV_TEST_VIEW(self.CLIENT))
-
-    @app_commands.command(name='vampire-img', description='Sets the character img for `vampire`')
-    @app_commands.describe(charactername='Character Name')
-    @app_commands.describe(characterimgurl='Character Image URL')
-    async def VampireImgSet(self, interaction: discord.Interaction, charactername: str, characterimgurl: str):
-        url_set_embed = Embed(title='URL Set', description='', color=mC.EMBED_COLORS["green"])
-        url_set_embed.add_field(name='Success', value='', inline=False)
-        targetDB = f'cogs//vampire//characters//{str(interaction.user.id)}//{charactername}//{charactername}.sqlite'
-        log.debug(f'> Checking if [ {targetDB} ] exist')
-        if not os.path.exists(targetDB):
-            log.warn(f'*> Database [ {targetDB} ] does not exist')
-
-            await interaction.response.send_message(embed=discord.Embed(
-                title='Database Error', color=mC.EMBED_COLORS["red"],
-                description=f'[ {charactername} ] Does Not Exist, Can\'t Set URL.'), ephemeral=True)
-
-            return False
-        else:
-            log.debug(f'> Successful Connection to [ {targetDB} ] Setting URL')
-
-        with sqlite3.connect(f'cogs//vampire//characters//{str(interaction.user.id)}//{charactername}//{charactername}.sqlite') as db:
-            db.cursor().execute('UPDATE charInfo SET imgURL=?', (f'{characterimgurl}',))
-            db.commit()
-        log.debug(f'> [ {targetDB} ] URL Set to [ {characterimgurl} ]')
-
-        with sqlite3.connect(f'cogs//vampire//characters//{str(interaction.user.id)}//{charactername}//{charactername}.sqlite') as db:
-            found_url = db.cursor().execute('SELECT imgURL from charInfo').fetchone()[0]
-            url_set_embed.set_thumbnail(url=f'{found_url}')
-        log.debug(f'> Sending [ {characterimgurl} ] back to user')
-
-        await interaction.response.send_message(embed=url_set_embed)
-
-    @commands.command(hidden=True)
-    async def new(self, ctx, targetcharacter: str):
-        # ! I swear this command will be cleaned eventually
-        if ctx.author.id == mC.RUNNER_ID:
-            try:
-                createdDirectory = f'{os.getcwd()}//cogs//vampire//characters//{str(ctx.author.id)}//{targetcharacter}'
-                log.debug(f'Creating Directory [ {createdDirectory} ] if not created')
-                os.makedirs(createdDirectory, exist_ok=True)
-                log.debug(f'Directory [ {createdDirectory} ] Created')
-
-                with sqlite3.connect(f'cogs//vampire//characters//{str(ctx.author.id)}//{targetcharacter}//{targetcharacter}.sqlite') as db:
-                    cursor = db.cursor()
-                    log.debug(f'Creation of [ {targetcharacter} ] Begun')
-                    # ? These were sorted by the listing order in DB Browser
-
-                    log.debug('Setting charAttributes')
-                    cursor.execute('CREATE TABLE IF NOT EXISTS charAttributes(strength INTEGER, dexterity INTEGER, stamina INTEGER, charisma INTEGER, manipulation INTEGER, composure INTEGER, intelligence INTEGER, wits INTEGER, resolve INTEGER)')
-                    cursor.execute('INSERT INTO charAttributes(strength, dexterity, stamina, charisma, manipulation, composure, intelligence, wits, resolve) VALUES(1,2,3, 1,2,3, 1,2,3)')
-
-                    log.debug('Setting charInfo')
-                    cursor.execute('CREATE TABLE IF NOT EXISTS charInfo(blood_potency INTEGER, clan TEXT, generation INTEGER, humanity INTEGER, bane_severity INTEGER, hunger INTEGER, predator_type TEXT, imgURL TEXT)')
-                    cursor.execute('INSERT INTO charInfo (blood_potency, clan, generation, humanity, bane_severity, hunger, predator_type, imgURL) VALUES(1, "ExampleClan", 10, 7, 2, 1, "nom", "https://cdn.discordapp.com/attachments/1145636416584429609/1181774388522143824/remix-be94809d-97fd-4654-9a6a-57b02abefb01.png?ex=658b82aa&is=65790daa&hm=39a5c62fdf1e8e0c43f8928b655fdb917fec7cb7807f702f4c257ab078ccd624&")')
-
-                    log.debug('Setting commandVars')
-                    cursor.execute('CREATE TABLE IF NOT EXISTS commandVars(difficulty INTEGER, rollPool INTEGER, result INTEGER, poolComp TEXT)')
-                    cursor.execute('INSERT INTO commandVars (difficulty, rollPool, result, poolComp) VALUES(0, 0, 0, "Stuff, And, Things")')
-
-                    log.debug('Setting disciplines')
-                    cursor.execute('CREATE TABLE IF NOT EXISTS disciplines('
-                                   'obfuscate INTEGER, animalism INTEGER, potence INTEGER, dominate INTEGER, auspex INTEGER, protean INTEGER, presence INTEGER, fortitude INTEGER, '
-                                   'thin_blood_alchemy INTEGER, blood_sorc INTEGER, chemeristry INTEGER, '
-                                   'seven INTEGER, myr INTEGER, selena INTEGER, nyct1 INTEGER, nyct2 INTEGER, iilta INTEGER, elijah INTEGER)')
-                    cursor.execute('INSERT INTO disciplines('
-                                   'obfuscate, animalism, potence, dominate, auspex, protean, presence, fortitude, '
-                                   'thin_blood_alchemy, blood_sorc, chemeristry, '
-                                   'seven, myr, selena, nyct1, nyct2, iilta, elijah)'
-                                   'VALUES(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1)')
-
-                    log.debug('Setting health')
-                    cursor.execute('CREATE TABLE IF NOT EXISTS health(healthBase INTEGER, healthSUP INTEGER, healthAGG INTEGER)')
-                    cursor.execute('INSERT INTO health (healthBase, healthSUP, healthAGG) VALUES(5,0,0)')
-
-                    log.debug('Setting mentalSkills')
-                    cursor.execute('CREATE TABLE IF NOT EXISTS mentalSkills(academics INTEGER, awareness INTEGER, finance INTEGER, investigation INTEGER, medicine INTEGER, occult INTEGER, politics INTEGER, science INTEGER, technology INTEGER)')
-                    cursor.execute('INSERT INTO mentalSkills (academics, awareness, finance, investigation, medicine, occult, politics, science, technology) VALUES(1,2,3,4,5,6,7,8,9)')
-
-                    log.debug('Setting ownerInfo')
-                    cursor.execute('CREATE TABLE IF NOT EXISTS ownerInfo(userID INTEGER, userNAME TEXT)')
-                    cursor.execute('INSERT INTO ownerInfo (userID, userNAME) VALUES(0, "nada")')
-                    cursor.execute('UPDATE ownerInfo SET userID=?, userNAME=?', (int(ctx.author.id), f"{ctx.author}"))
-
-                    log.debug('Setting physicalSkills')
-                    cursor.execute('CREATE TABLE IF NOT EXISTS physicalSkills(athletics INTEGER, brawl INTEGER, craft INTEGER, drive INTEGER, firearms INTEGER, larceny INTEGER, melee INTEGER, stealth INTEGER, survival INTEGER)')
-                    cursor.execute('INSERT INTO physicalSkills (athletics, brawl, craft, drive, firearms, larceny, melee, stealth, survival) VALUES(1,2,3,4,5,6,7,8,9)')
-
-                    log.debug('Setting rerollInfo')
-                    cursor.execute('CREATE TABLE IF NOT EXISTS rerollInfo(regularCritDie INTEGER, hungerCritDie INTEGER, regularSuccess INTEGER, hungerSuccess INTEGER, regularFail INTEGER, hungerFail INTEGER, hungerSkull INTEGER)')
-                    cursor.execute('INSERT INTO rerollInfo (regularCritDie, hungerCritDie, regularSuccess, hungerSuccess, regularFail, hungerFail, hungerSkull) VALUES(0,0, 0,0, 0,0, 0)')
-
-                    log.debug('Setting socialSkills')
-                    cursor.execute('CREATE TABLE IF NOT EXISTS socialSkills(animal_ken INTEGER, etiquette INTEGER, insight INTEGER, intimidation INTEGER, leadership INTEGER, performance INTEGER, persuasion INTEGER, streetwise INTEGER, subterfuge INTEGER)')
-                    cursor.execute('INSERT INTO socialSkills (animal_ken, etiquette, insight, intimidation, leadership, performance, persuasion, streetwise, subterfuge) VALUES(1,2,3,4,5,6,7,8,9)')
-
-                    log.debug('Setting willpower')
-                    cursor.execute('CREATE TABLE IF NOT EXISTS willpower(willpowerBase INTEGER, willpowerSUP INTEGER, willpowerAGG INTEGER)')
-                    cursor.execute('INSERT INTO willpower (willpowerBase, willpowerSUP, willpowerAGG) VALUES(5,0,0)')
-
-                    db.commit()
-                    db.close()
-
-                    log.debug(f'Creation of [ {targetcharacter} ] Successful')
-                    await ctx.send('Make Complete')
-            except sqlite3.Error as e:
-                log.error(f'vampMaker | SQLITE3 ERROR | {e}')
 
 
 async def setup(CLIENT):
--- cogs/vtm_toolbox/vtb_characters/vtb_character_manager.py
+++ cogs/vtm_toolbox/vtb_characters/vtb_character_manager.py
@@ -8,12 +8,121 @@ from discord.ui import View
 from misc.config import mainConfig as mC
 
 
-async def make_character_file(interaction: discord.Interaction, character_name):
-    CHARACTER_FILE: str = f'cogs/vtm_toolbox/vtb_characters/{interaction.user.id}/{character_name}/{character_name}.json'
-    CHARACTER_DICT: dict = {'character_name': character_name, 'owner_id': interaction.user.id}
+async def make_character_files(interaction: discord.Interaction, character_name):
+    CHARACTER_DIRECTORY: str = f'cogs/vtm_toolbox/vtb_characters/{interaction.user.id}/{character_name}/'
 
-    with open(CHARACTER_FILE, "w") as operate_file:
-        json.dump(CHARACTER_DICT, operate_file)
+    os.mkdir(f'cogs/vtm_toolbox/vtb_characters/{interaction.user.id}')
+    os.mkdir(f'{CHARACTER_DIRECTORY}')
+    os.mkdir(f'{CHARACTER_DIRECTORY}skills')
+    os.mkdir(f'{CHARACTER_DIRECTORY}roll')
+
+    MISC_DICT: dict = {'owner_id'            : interaction.user.id,
+                       'character_name'      : character_name,
+                       'blood_potency'       : 0,
+                       'clan'                : 'UNSET',
+                       'generation'          : 0,
+                       'humanity'            : 0,
+                       'bane_severity'       : 0,
+                       'hunger'              : 0,
+                       'predator_type'       : 'UNSET',
+                       'character_avatar_url': mC.PLACEHOLDER_IMG}
+    HEALTH_DICT: dict = {'base_health'              : 0,
+                         'superficial_health_damage': 0,
+                         'aggravated_health_damage' : 0}
+    WILLPOWER_DICT: dict = {'base_willpower'              : 0,
+                            'superficial_willpower_damage': 0,
+                            'aggravated_willpower_damage' : 0}
+    ATTRIBUTES_DICT: dict = {'strength'    : 0,
+                             'dexterity'   : 0,
+                             'stamina'     : 0,
+                             'charisma'    : 0,
+                             'manipulation': 0,
+                             'composure'   : 0,
+                             'intelligence': 0,
+                             'wits'        : 0,
+                             'resolve'     : 0}
+    DISCIPLINES_DICT: dict = {'obfuscate'          : 0,
+                              'animalism'          : 0,
+                              'potence'            : 0,
+                              'dominate'           : 0,
+                              'auspex'             : 0,
+                              'protean'            : 0,
+                              'presence'           : 0,
+                              'fortitude'          : 0,
+                              'thin_blood_alchemy' : 0,
+                              'blood_sorcery'      : 0,
+                              'chemeristry'        : 0,
+                              'seven_specific'     : 0,
+                              'myr_specific'       : 0,
+                              'selena_specific'    : 0,
+                              'nyctea_specific_one': 0,
+                              'nyctea_specific_two': 0,
+                              'elijah_specific'    : 0}
+    PHYSICAL_SKILLS_DICT: dict = {'athletics': 0,
+                                  'brawl'    : 0,
+                                  'craft'    : 0,
+                                  'drive'    : 0,
+                                  'firearms' : 0,
+                                  'larceny'  : 0,
+                                  'melee'    : 0,
+                                  'stealth'  : 0,
+                                  'survival' : 0}
+    SOCIAL_SKILLS_DICT: dict = {'animal_ken'  : 0,
+                                'etiquette'   : 0,
+                                'insight'     : 0,
+                                'intimidation': 0,
+                                'leadership'  : 0,
+                                'performance' : 0,
+                                'persuasion'  : 0,
+                                'streetwise'  : 0,
+                                'subterfuge'  : 0}
+    MENTAL_SKILLS_DICT: dict = {'academics'    : 0,
+                                'awareness'    : 0,
+                                'finance'      : 0,
+                                'investigation': 0,
+                                'medicine'     : 0,
+                                'occult'       : 0,
+                                'politics'     : 0,
+                                'science'      : 0,
+                                'technology'   : 0}
+    ROLL_DICT: dict = {'difficulty': 0,
+                       'pool' : 0,
+                       'result': '',
+                       'composition': (),
+                       'regular_crit_count': 0,
+                       'regular_success_count': 0,
+                       'regular_fail_count': 0,
+                       'hunger_crit_count': 0,
+                       'hunger_success_count' : 0,
+                       'hunger_fail_count' : 0,
+                       'skull_count': 0}
+
+    with open(f'{CHARACTER_DIRECTORY}misc.json', "w") as operate_file:
+        json.dump(MISC_DICT, operate_file)
+
+    with open(f'{CHARACTER_DIRECTORY}health.json', "w") as operate_file:
+        json.dump(HEALTH_DICT, operate_file)
+
+    with open(f'{CHARACTER_DIRECTORY}willpower.json', "w") as operate_file:
+        json.dump(WILLPOWER_DICT, operate_file)
+
+    with open(f'{CHARACTER_DIRECTORY}attributes.json', "w") as operate_file:
+        json.dump(ATTRIBUTES_DICT, operate_file)
+
+    with open(f'{CHARACTER_DIRECTORY}disciplines.json', "w") as operate_file:
+        json.dump(DISCIPLINES_DICT, operate_file)
+
+    with open(f'{CHARACTER_DIRECTORY}skills/physical.json', "w") as operate_file:
+        json.dump(PHYSICAL_SKILLS_DICT, operate_file)
+
+    with open(f'{CHARACTER_DIRECTORY}skills/social.json', "w") as operate_file:
+        json.dump(SOCIAL_SKILLS_DICT, operate_file)
+
+    with open(f'{CHARACTER_DIRECTORY}skills/mental.json', "w") as operate_file:
+        json.dump(MENTAL_SKILLS_DICT, operate_file)
+
+    with open(f'{CHARACTER_DIRECTORY}roll/info.json', "w") as operate_file:
+        json.dump(ROLL_DICT, operate_file)
 
 
 class vtb_DEV_TEST_VIEW(View):
@@ -23,20 +132,42 @@ class vtb_DEV_TEST_VIEW(View):
 
     @discord.ui.button(label='TESTING', emoji='<:ExodusE:1145153679155007600>', style=discord.ButtonStyle.green, row=1)
     async def dev_test_button_one_button_callback(self, interaction, button):
-
-        character_class = vtb_CharacterManager(interaction)
+        character_class = vtb_Character(interaction)
 
         dev_test_embed = discord.Embed(title='`!__DEV__DEBUG__TESTS__!`',
                                        description='`!__ONLY__PRESS__THINGS__IF__INSTRUCTED__!`',
                                        color=mC.EMBED_COLORS['red'])
         dev_test_embed.add_field(name='Char Name', value=f'{character_class.CHARACTER_NAME}', inline=True)
         dev_test_embed.add_field(name='Char Owner ID', value=f'{character_class.OWNER_ID}', inline=True)
-        dev_test_embed.add_field(name='Interactor ID', value=f'{interaction.user.id}', inline=True)
+
+        wanted_information = ('base_willpower',)
+        character_information: dict = await character_class.__get_information__(wanted_information, 'willpower')
+        dev_test_embed.add_field(name='Char Willpower', value=f'{character_information[wanted_information[0]]}', inline=True)
+
+        wanted_information = ('science', 'occult')
+        character_information: dict = await character_class.__get_information__(wanted_information, 'skills/mental')
+        dev_test_embed.add_field(name='Char Science', value=f'{character_information[wanted_information[0]]}', inline=True)
+        dev_test_embed.add_field(name='Char Occult', value=f'{character_information[wanted_information[1]]}', inline=True)
+
+        new_information = (('base_willpower',), (2,))
+        await character_class.__update_information__(new_information, 'willpower')
+
+        new_information = (('science', 'occult'), (5, 9))
+        await character_class.__update_information__(new_information, 'skills/mental')
+
+        wanted_information = ('base_willpower',)
+        character_information: dict = await character_class.__get_information__(wanted_information, 'willpower')
+        dev_test_embed.add_field(name='Char Willpower 2', value=f'{character_information[wanted_information[0]]}', inline=True)
+
+        wanted_information = ('science', 'occult')
+        character_information: dict = await character_class.__get_information__(wanted_information, 'skills/mental')
+        dev_test_embed.add_field(name='Char Science 2', value=f'{character_information[wanted_information[0]]}', inline=True)
+        dev_test_embed.add_field(name='Char Occult 2', value=f'{character_information[wanted_information[1]]}', inline=True)
 
         await interaction.response.send_message(embed=dev_test_embed, view=vtb_DEV_TEST_VIEW(self.CLIENT))
 
 
-class vtb_CharacterManager:
+class vtb_Character:
     def __init__(self, interaction: discord.Interaction):
         CHARACTER_NAME_FILE: str = f'cogs/vtm_toolbox/vtb_characters/{interaction.user.id}/target_character.json'
         with open(CHARACTER_NAME_FILE, 'r') as operate_file:
@@ -44,13 +175,13 @@ class vtb_CharacterManager:
 
         self.CHARACTER_NAME: str = CHARACTER_NAME
 
-        CHARACTER_FILE_PATH: str = f'cogs/vtm_toolbox/vtb_characters/{interaction.user.id}/{self.CHARACTER_NAME}/{self.CHARACTER_NAME}.json'
+        self.CHARACTER_FILE_PATH: str = f'cogs/vtm_toolbox/vtb_characters/{interaction.user.id}/{self.CHARACTER_NAME}'
 
-        if not os.path.isfile(CHARACTER_FILE_PATH):
+        if not os.path.isdir(self.CHARACTER_FILE_PATH):
             # This just means the interactor doesn't have any character with the given name.
             raise FileNotFoundError
 
-        with open(CHARACTER_FILE_PATH, 'r') as operate_file:
+        with open(f'{self.CHARACTER_FILE_PATH}/misc.json', 'r') as operate_file:
             CHARACTER_INFO = json.load(operate_file)
 
         if int(CHARACTER_INFO['owner_id']) == interaction.user.id:
@@ -58,3 +189,33 @@ class vtb_CharacterManager:
         else:
             log.error('*> Bad Character Owner')
             raise Exception('Bad Character Owner')
+
+    async def __get_information__(self, WANTED_INFORMATION: tuple, FILE_NAME: str) -> dict:
+        with open(f'{self.CHARACTER_FILE_PATH}/{FILE_NAME}.json', 'r') as operate_file:
+            CHARACTER_INFO: dict = json.load(operate_file)
+
+        return_information: dict = {}
+        loop_counter: int = 0
+        for x in WANTED_INFORMATION:
+            return_information[f'{WANTED_INFORMATION[loop_counter]}'] = CHARACTER_INFO[f'{WANTED_INFORMATION[loop_counter]}']
+            loop_counter += 1
+
+        return return_information
+
+    async def __update_information__(self, NEW_INFORMATION: tuple, FILE_NAME: str) -> dict:
+        with open(f'{self.CHARACTER_FILE_PATH}/{FILE_NAME}.json', 'r') as operate_file:
+            character_info: dict = json.load(operate_file)
+
+        return_information: dict = {}
+        loop_counter: int = 0
+        for x in NEW_INFORMATION[0]:
+            dict_key = NEW_INFORMATION[0][loop_counter]
+            new_value = NEW_INFORMATION[1][loop_counter]
+            log.crit(f'{dict_key} -> {new_value}')
+            character_info[dict_key] = new_value
+            loop_counter += 1
+
+        with open(f'{self.CHARACTER_FILE_PATH}/{FILE_NAME}.json', 'w') as operate_file:
+            json.dump(character_info, operate_file)
+
+        return return_information
