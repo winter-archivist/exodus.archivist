ec52743c70499e6d4c2fd88701f71ffc4b3fab95
AshenEntropy <ashen_entropy@proton.me>
2024-06-21 05:20:59 -0400

    01.06.05 | overseer added to initial cogs

--- ea_client.py
+++ ea_client.py
@@ -19,7 +19,8 @@ async def initialize_startup_cogs(CLIENT_INPUT):
 
     # To add cogs, just add their directory, but instead of "/" use ".", however do not include their file extension.
     INITIAL_COGS: tuple = ('cogs.cog_manager',
-                           'cogs.vtm_toolbox.vampire_toolbox_cog')
+                           'cogs.vtm_toolbox.vampire_toolbox_cog',
+                           'cogs.overseer.overseer_cog')
 
     # Tries to load any cog listed in the above tuple; stops execution if the cog isn't found.
     for_var = 0
